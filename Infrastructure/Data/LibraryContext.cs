6c7113efd0b7f7899025c0632c0fe083d0c5f109
Daniel Johansson <johansson.gd@gmail.com>
2021-04-12 19:17:25 +0200

    lagt till EF Core

---
+++ Infrastructure/Data/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
